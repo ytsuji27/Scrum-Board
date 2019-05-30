db61014b757dafe0c9c0e2f21e9216027006260f
ytsuji27 <ytsuji91@gmail.com>
2019-05-29 17:32:46 -0700

    Fixed frontend bug

--- scrum-board-frontend/src/constants.js
+++ scrum-board-frontend/src/constants.js
@@ -1,13 +1,25 @@
 // ######### URLs ######### //
-export const PROFILE_URL = 'http://localhost:3000/api/v1/profile'; 
-export const LOGIN_URL = 'http://localhost:3000/api/v1/login';
-export const USERS_URL = 'http://localhost:3000/api/v1/users';
-export const PROJECTS_URL = 'http://localhost:3000/api/v1/projects';
-export const USERPROJECTS_URL = 'http://localhost:3000/api/v1/userproject';
-export const TASKS_URL = 'http://localhost:3000/api/v1/tasks';
-export const PROJECTTASKS_URL = 'http://localhost:3000/api/v1/projecttasks';
-export const PROJECTCATEGORIES_URL = 'http://localhost:3000/api/v1/projectcategory';
-export const CATEGORIES_URL = 'http://localhost:3000/api/v1/categories';
+// # FOR LOCAL DEPLOYMENT # //
+// export const PROFILE_URL = 'http://localhost:3000/api/v1/profile'; 
+// export const LOGIN_URL = 'http://localhost:3000/api/v1/login';
+// export const USERS_URL = 'http://localhost:3000/api/v1/users';
+// export const PROJECTS_URL = 'http://localhost:3000/api/v1/projects';
+// export const USERPROJECTS_URL = 'http://localhost:3000/api/v1/userproject';
+// export const TASKS_URL = 'http://localhost:3000/api/v1/tasks';
+// export const PROJECTTASKS_URL = 'http://localhost:3000/api/v1/projecttasks';
+// export const PROJECTCATEGORIES_URL = 'http://localhost:3000/api/v1/projectcategory';
+// export const CATEGORIES_URL = 'http://localhost:3000/api/v1/categories';
+
+// # FOR ONLINE DEPLOYMENT # //
+export const PROFILE_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/profile'; 
+export const LOGIN_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/login';
+export const USERS_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/users';
+export const PROJECTS_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/projects';
+export const USERPROJECTS_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/userproject';
+export const TASKS_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/tasks';
+export const PROJECTTASKS_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/projecttasks';
+export const PROJECTCATEGORIES_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/projectcategory';
+export const CATEGORIES_URL = 'https://sleepy-sierra-79170.herokuapp.com/api/v1/categories';
 
 // ######### OTHER ######### //
 export const HEADERBODY = {
